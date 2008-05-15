408b88744f1384202a464e808887b50af1e86898
Gregory Burd <gburd@ossus.com>
2008-05-15 17:15:17 -0400

    first commit

---
+++ version.h
@@ -0,0 +1,9 @@
+/* version.h - version defines for thttpd and libhttpd */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define SERVER_SOFTWARE "dbxmld/0.0 29dec2003"
+#define SERVER_ADDRESS "http://www.oracle.com/"
+
+#endif /* _VERSION_H_ */
---
+++ xm_get.c
@@ -0,0 +1,4 @@
+/* implement a HTTP/GET into the BDB XML database */
+http://exist.sourceforge.net/devguide.html
+http://www.orbeon.com/ops/doc/processors-xmldb
+http://www.xquery.com/compare/comparison.html
