7f8d7d19c21c0af45d2c563f31af85c823d0ad85
Gregory Burd <gburd@ossus.com>
2010-11-12 13:58:16 -0500

    From open to proprietary, from acme/libevent to Boost ASIO.

--- tdate_parse.h
+++
@@ -1,9 +0,0 @@
-/* tdate_parse.h - parse string dates into internal form, stripped-down version
- */
-
-#ifndef _TDATE_PARSE_H_
-#define _TDATE_PARSE_H_
-
-extern time_t tdate_parse( char* str );
-
-#endif /* _TDATE_PARSE_H_ */
